6bc34ebda667164d76e15b1a1b12196f8eb6c367
Sergey Kudashev <kudashevs@gmail.com>
2024-11-27 10:28:07 +0300

    Style adapter remove a redundant comment

--- test/helpers/adapter.js
+++ test/helpers/adapter.js
@@ -2,7 +2,6 @@ import VitePluginRemoveBlock from "../../src/plugin.js";
 import * as path from "node:path";
 import {fileURLToPath} from 'node:url';
 
-// https://stackoverflow.com/questions/70009442/parsing-error-unexpected-token-import-when-accessing-import-meta-url-cloud9
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const filePath = path.resolve(__dirname, '../any.css');
 
